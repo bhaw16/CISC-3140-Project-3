01b6ec3fc8a28cdd00aeda193cdac8a947e66270
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2024-08-11 19:41:01 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,26 +1,40 @@
 var express = require("express");
+var router = express.Router();
 var app = express();
 var http = require("http");
-var server = http.createServer(app);
-var { join } = require("node:path");
+var server = http.createServer(app, (req, res) => {
+    res.setHeader("Content Type", "application/javascript");
+});
+//var { join } = require("node:path");
 var { Server } = require("socket.io");  //Server module
 var io = new Server(server);
 var path = require("path");
-path.basename("/Users/briannahawkins/Desktop/cisc-3140-project-3/");
+path.basename("/Users/briannahawkins/Desktop/cisc-3140-project-3/client");
+
+app.use = (req, res, next) => {
+    //console.log(req.method);
+    next = () => console.log("we have reached this code");
+    next();
+}
+
+app.use(express.static("js"));
+
 
 app.get('/', (req, res) => {
-    res.sendFile(app.use(express.static(path.join(__dirname, "client")), "index.html"), (err) => {
+    res.sendFile("index.html", {root: "/Users/briannahawkins/Desktop/cisc-3140-project-3/client/"}), (err) => {
         if (err) {
-            res.writeHead(404, "Content Type: text/html");
-            return res.end("404 Not Found");
+            console.log("Error sending HTML file.");
         }
-    });
+        else {
+            console.log("It's working! :)");
+        }
+    }
 });
 
 io.on("connection", (socket) => {
-    console.log(`A user connected at ${new Date().toLocaleTimeString}.`);
+    console.log(`A user connected at ${new Date().toLocaleTimeString()}.`);
     socket.on("disconnect", () => {
-        console.log(`A user disconnected at ${new Date().toLocaleTimeString}.`);
+        console.log(`A user disconnected at ${new Date().toLocaleTimeString()}.`);
     })
 })
 
@@ -28,4 +42,4 @@ server.listen(3000, () => {
     console.log("server listening on port 3000");
 });
 module.exports = app;
-return this.router;
+module.exports = router;
