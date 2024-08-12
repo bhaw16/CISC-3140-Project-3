87ac08623a79d1a6bc5a3921031162b1d6f385f1
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2024-08-11 22:04:15 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -9,19 +9,24 @@ var server = http.createServer(app, (req, res) => {
 var { Server } = require("socket.io");  //Server module
 var io = new Server(server);
 var path = require("path");
-path.basename("/Users/briannahawkins/Desktop/cisc-3140-project-3/client");
+var pathString;
+//path.basename("/Users/briannahawkins/Desktop/cisc-3140-project-3/client");
 
 app.use = (req, res, next) => {
     //console.log(req.method);
-    next = () => console.log("we have reached this code");
+    next = () => {
+        pathString = `${__dirname.substring(0, __dirname.indexOf("server"))}${"client/index.html"}`;
+        console.log(pathString);
+    };
     next();
 }
 
 app.use(express.static("js"));
+app.use('/', express.static(path.join(pathString)));
 
-
+//"index.html", {root: "/Users/briannahawkins/Desktop/cisc-3140-project-3/client/"}
 app.get('/', (req, res) => {
-    res.sendFile("index.html", {root: "/Users/briannahawkins/Desktop/cisc-3140-project-3/client/"}), (err) => {
+    res.sendFile(path.join(pathString)), (err) => {
         if (err) {
             console.log("Error sending HTML file.");
         }
