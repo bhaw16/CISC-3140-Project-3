8a76502a44295d5e9e5e2f601b1aed00392bfb0c
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2024-08-14 23:16:07 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ var http = require("http");
 var server = http.createServer(app, (req, res) => {
     res.setHeader("Content Type", "application/javascript");
 });
-//var { join } = require("node:path");
+var { join } = require("node:path");
 var { Server } = require("socket.io");  //Server module
 var io = new Server(server);
 var path = require("path");
+//var clientPath = path.join(__dirname, "/../client");
 var pathString;
 //path.basename("/Users/briannahawkins/Desktop/cisc-3140-project-3/client");
 
@@ -22,13 +23,15 @@ app.use = (req, res, next) => {
 }
 
 app.use(express.static("js"));
-app.use('/', express.static(path.join(pathString)));
+//app.use(express.static(path.join(pathString)));
+
 
 //"index.html", {root: "/Users/briannahawkins/Desktop/cisc-3140-project-3/client/"}
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.sendFile(path.join(pathString)), (err) => {
         if (err) {
             console.log("Error sending HTML file.");
+            next(err);
         }
         else {
             console.log("It's working! :)");
@@ -38,10 +41,14 @@ app.get('/', (req, res) => {
 
 io.on("connection", (socket) => {
     console.log(`A user connected at ${new Date().toLocaleTimeString()}.`);
+    socket.on("chat message", (msg) => {
+        console.log(`${msg}`);
+        io.emit("chat message", msg);
+    })
     socket.on("disconnect", () => {
         console.log(`A user disconnected at ${new Date().toLocaleTimeString()}.`);
     })
-})
+});
 
 server.listen(3000, () => {
     console.log("server listening on port 3000");
