1dd5b1502d8f0a49d54c71487b4f815da5c84681
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2024-08-14 23:15:32 -0400

    Add files via upload

--- client/script.js
+++ client/script.js
@@ -1,28 +1,54 @@
 window.onload = () => {
-    var submitButton = document.getElementsByName("submit")[0];
+    /*
+    var script = document.createElement("script");
+    script.setAttribute("src", "/socket.io/socket.io.js");
+    */
+    var form = document.getElementsByTagName("form")[0];
+    var input = document.getElementsByName("message")[0];
     var numMessagesSent = 0;
     var numMessagesChanged = true;
+    var io = require("socket.io-client");
+    const socket = io('http://localhost:5000');
+    console.log("we have reached this code");
 
-    submitButton.addEventListener("click", onSend = () => {
+    socket.on("connect", () => {
+        const engine = socket.io.engine;
+        console.log(engine.transport.name);
+    });
+
+    submitButton.addEventListener("submit", onSend = () => {
+        socket.emit("Hello");
+        /*
         try {
             if (numMessagesChanged) {
-                document.getElementsByTagName("form")[0].insertAdjacentElement("afterbegin", document.createElement("p"));
+                form.insertAdjacentElement("afterbegin", document.createElement("p"));
             }
             console.log(document.getElementsByTagName("p"));
             document.getElementsByTagName("p")[numMessagesSent].innerText = "";
-            if (document.getElementsByName("message")[0].getAttribute("value") == null) {
+            e.preventDefault();
+            if (input.value == null) {
                 throw new Error("Message body is empty.");
             }
             else {
-                document.getElementsByTagName("p")[numMessagesSent].innerText = document.getElementsByName("message")[0].getAttribute("value");
+                //document.getElementsByTagName("p")[numMessagesSent].innerText = input.value;
                 alert("Message sent!");
                 numMessagesChanged = true;
-                numMessagesSent++;
+                //numMessagesSent++;
             }
         }
         catch (err) {
             document.getElementsByTagName("p")[numMessagesSent].innerText = err.message;
             numMessagesChanged = false;
         }
-    })
+        */
+    });
+
+    
+
+    socket.on("chat message", (msg) => {
+        if (numMessagesChanged) {
+            document.getElementsByTagName("p")[numMessagesSent].innerText = msg;
+            numMessagesSent++;
+        }
+    });
 }
